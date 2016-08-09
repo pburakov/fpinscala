d702f7962da596a9e4e3a7fa951821224fc51951
Paul Burakov <pburakov@gmail.com>
2016-08-08 21:49:55 -0400

    dropWhile

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -65,7 +65,11 @@ object List { // `List` companion object. Contains functions for creating and wo
     if (n > 0) drop(tail(l), n - 1)
     else l
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  @annotation.tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(h, t) if f(h) => dropWhile(t, f)
+    case _ => l
+  }
 
   def init[A](l: List[A]): List[A] = sys.error("todo")
 
