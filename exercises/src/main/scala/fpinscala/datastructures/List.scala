c62cf6720a9ff0cc20ef2bf66b6552c1294ccfa3
Paul Burakov <pburakov@gmail.com>
2016-08-08 21:46:55 -0400

    ident setHead

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,13 +50,15 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] =
-    l match {
-      case Nil => l
-      case Cons(_, t) => t
-    }
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => l
+    case Cons(_, t) => t
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons(h, Nil)
+    case Cons(_, t) => Cons(h, t)
+  }
 
   def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
 
