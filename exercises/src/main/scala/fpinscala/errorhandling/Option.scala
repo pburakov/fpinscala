13706d39b89ff6f31afc80981875f408b2d76b3e
Paul Burakov <pburakov@gmail.com>
2016-08-11 21:32:37 -0400

    sequence

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -62,7 +62,10 @@ object Option {
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     a.flatMap(a => b.map(b => f(a, b)))
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h.flatMap(h => sequence(t).map(h :: _))
+  }
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
 }
