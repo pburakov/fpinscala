48393b82cb1f11d29b74f60937023f3d28927eb9
Paul Burakov <pburakov@gmail.com>
2016-09-05 14:34:03 -0400

    map2

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -19,7 +19,12 @@ sealed trait Either[+E,+A] {
    case Right(a) => Right(a)
  }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+   for {
+     a <- this
+     bb <- b
+   } yield f(a, bb)
+ }
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
